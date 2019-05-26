6535f73dd12438c90aaf68976ff4f330a316102c
Konrad <dziaaq1994@gmail.com>
2019-05-26 11:03:27 +0200

    Add some code

--- Multithreading/Multithreading.cpp
+++ Multithreading/Multithreading.cpp
@@ -4,20 +4,33 @@
 
 using namespace std;
 
+static bool s_finished = false;
 
 void do_work()
 {
+    using namespace std::literals::chrono_literals;
 
+    cout << "Started thread, ID: " << this_thread::get_id() << endl;
+
+    while(!s_finished)
+    {
+        cout << "Working..." << endl;
+        this_thread::sleep_for(1s);
+    }
 }
 
 
 int main()
 {
-	thread worker(do_work); // execute thread function
+    thread worker(do_work); // execute thread function
 
-	worker.join(); // wait for the thread to finish (close thread)
+    cin.get();
+    s_finished = true;
 
+    worker.join(); // wait for the thread to finish (close thread)
 
+    cout << "Started thread, ID: " << this_thread::get_id() << endl;
+    cout << "Finished" << endl;
 
-	cin.get();
+    cin.get(); // wait untill ENTER is pressed
 }
